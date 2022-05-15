066362bd78aa750a7bf626b917cf0b17c9dbae22
Luca <s1094518@studenti.univpm.it>
2022-05-15 18:12:03 +0200

    prime aggiunte

---
+++ laraProj6/resources/views/footer/_footer.blade.php
@@ -0,0 +1,38 @@
+     <div class="row">
+
+         <div id="social" class="twelve columns">
+
+            <div class="bgrid-halves" >
+                 <div class="columns align-center-div" >
+                     <div>
+			<h5>Address and Phone</h5>
+                	<p class="address">
+                            Sparrow Studio<br>
+                            1600 Amphitheatre Parkway <br>
+                            Mountain View, CA 94043 US<br>
+                            <span>(123) 456-7890</span>
+                        </p>
+                     </div>
+                 </div>
+                 <div class="columns align-center-div">
+                     <div>
+                    <h5>Email and Social</h5>
+                    <p>
+                     E-mail: info{@}sparrow.com<br>
+                     Twitter: <a href="#">@sparrow</a><br>
+                     Facebook: <a href="#">sparrow FB page</a><br>
+                     Google+: <a href="#">sparrow G+ page</a>
+                    </p>
+                     </div>
+                  </div>
+            </div>
+             </div>
+            <ul class="copyright">
+               <li>Copyright &copy; 2014 Sparrow</li>
+               <li>Design by <a title="Styleshout" href="#">Styleshout</a></li>
+            </ul>
+
+         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+      </div>
+ <!-- Footer End-->
