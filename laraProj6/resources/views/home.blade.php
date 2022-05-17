99528fef5dbd209dde0dd6680456153904a146e0
Alex Rinaldi <s1096206@studenti.univpm.it>
2022-05-17 10:18:48 +0200

    Aggiunta regolamento d'uso

--- laraProj6/resources/views/home.blade.php
+++ laraProj6/resources/views/home.blade.php
@@ -158,8 +158,23 @@ endguest
        <div class="row">
          <div class="twelve columns align-center">
             <p>
-           La nostra azienda 'Airdrop',
-       </p>
+                1. Una volta entrati nel sito, è preferibile creare un account ed effettuare l'accesso. <br>
+                2. E' possibile accedere al catalogo degli annunci e navigare fra di essi anche senza effettuare l'accesso <br>
+                3. All'interno della form di Registrazione, vi è la necessità di decidere la tipologia dell'account
+                da creare in base alle esigenze dell'utente. (Creare annunci o richiedere di prenotarli) <br>
+                4. Se si accede come locatori, verrà caricata una Homepage specifica dalla quale è possibile
+                gestire gli annunci personali e visualizzare il catalogo.
+                5. Se si accede come studenti, verrà caricata una Homepage specifica dalla quale ci si può muovere 
+                all'interno dei vari annunci nel catalogo e presentare delle richieste di prenotazione. <br>
+                6. Attraverso le form contenenti gli annunci prenotabili per gli studenti, le richieste di prenotazione pervenute
+                ai locatori e il sistema di messaggistica, è possibile inviare messaggi ad utenti specifici.<br>
+                7. E' presente un sistema di messaggistica raggiungibile dalle varie Homepage specifiche cliccando sull'apposito
+                bottone, all'interno si sviluppa in varie parti: messaggi ricevuti, messaggi inviati, descrizione di un messaggio
+                e l'invio di un nuovo messaggio. <br>
+                8. Una volta effettuato l'accesso, dalla barra di navigazione in alto è possibile scorrere fino all'icona di un omino
+                rappresentante la gestione profilo. Cliccando su di esso è possibile aprire il profilo personale, effettuare la disconnessione
+                oppure visualizzare le FAQS.
+            </p>
          </div>
       </div>
        
