e98b6a5b4ec4c8ef4431d2d34a9e792b457d3699
Luca <s1094518@studenti.univpm.it>
2022-05-18 15:25:13 +0200

    footer provvisorio

--- laraProj6/resources/views/footer/_footer.blade.php
+++ laraProj6/resources/views/footer/_footer.blade.php
@@ -5,9 +5,9 @@
             <div class="bgrid-halves" >
                  <div class="columns align-center-div" >
                      <div>
-			<h5>Address and Phone</h5>
+			<h5>Indirizzo e Recapiti</h5>
                 	<p class="address">
-                            Sparrow Studio<br>
+                            HOUSTUDENT<br>
                             1600 Amphitheatre Parkway <br>
                             Mountain View, CA 94043 US<br>
                             <span>(123) 456-7890</span>
@@ -16,20 +16,20 @@
                  </div>
                  <div class="columns align-center-div">
                      <div>
-                    <h5>Email and Social</h5>
+                    <h5>Email e Social</h5>
                     <p>
-                     E-mail: info{@}sparrow.com<br>
-                     Twitter: <a href="#">@sparrow</a><br>
-                     Facebook: <a href="#">sparrow FB page</a><br>
-                     Google+: <a href="#">sparrow G+ page</a>
+                     E-mail: info{@}houstudent.com<br>
+                     Twitter: <a href="#">@houstudent</a><br>
+                     Facebook: <a href="#">houstudent FB page</a><br>
+                     Google+: <a href="#">houstudent G+ page</a>
                     </p>
                      </div>
                   </div>
             </div>
              </div>
             <ul class="copyright">
-               <li>Copyright &copy; 2014 Sparrow</li>
-               <li>Design by <a title="Styleshout" href="#">Styleshout</a></li>
+               <li>Copyright &copy; 2022 Houstudent</li>
+               <li>Design by <a title="Styleshout" href="#">us</a></li>
             </ul>
 
          <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
