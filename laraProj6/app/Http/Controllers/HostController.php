ae85b0e07ca0903a7d4b952b9b7b5bb31de96e5f
Alex Rinaldi <s1096206@studenti.univpm.it>
2022-05-20 19:22:35 +0200

    inserimento prova insertacc su navbar + host controller

---
+++ laraProj6/app/Http/Controllers/HostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Resources\Accomodation;
+use App\Models\Resources\Message;
+use App\Models\Resources\Option;
+
+use Illuminate\Http\Request;
+
+class HostController extends Controller {
+    public function prova1 (){
+        return view('accommodation/insertAcc');
+
+    }
+    }
+
--- laraProj6/resources/views/navs/_navpublic.blade.php
+++ laraProj6/resources/views/navs/_navpublic.blade.php
@@ -3,4 +3,5 @@
     <li class="noCurrent"><a href="{{ route('catalogo') }}" title="Catalogo degli alloggi">Catalogo</a></li>
     <li class="noCurrent"><a href="{{ route('login') }}" title="Login">Login</a></li>
     <li class="noCurrent"><a href="{{ route('register') }}" title="Registrazione">Registrati</a></li>
+    <li class="noCurrent"><a href="{{ route('insertAcc') }}" title="Inserimento offerta">Inserimento offerta</a></li>
 </ul>
