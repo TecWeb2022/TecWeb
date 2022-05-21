6ac010f0a4c87ca2348d37916d2b1ffcb1b99c61
Luca <s1094518@studenti.univpm.it>
2022-05-21 13:10:14 +0200

    finita visualizzazione accommodation, manca il js per bottone

---
+++ laraProj6/resources/views/helpers/tipologiaAcc.blade.php
@@ -0,0 +1,17 @@
+@php
+   
+    $descr = '';
+    switch($acc->tipologia) {
+        case 'ap':
+            $descr = "Appartamento";
+            break;
+        case 'cs':
+            $descr = "Posto letto in camera singola";
+            break;
+        case 'cd':
+            $descr = "Posto letto in camera doppia";
+            break;
+}
+
+@endphp
+<span class="testo">{{ $descr }}</span>
