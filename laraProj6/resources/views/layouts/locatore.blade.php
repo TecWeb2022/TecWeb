6b95b0cbaefcdbec44c02dcf1df2c6d1b1fec474
Luca <s1094518@studenti.univpm.it>
2022-05-23 15:09:50 +0200

    layout locatore

--- laraProj6/resources/views/layouts/locatore.blade.php
+++ laraProj6/resources/views/layouts/locatore.blade.php
@@ -1,16 +1,44 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/default.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/media-queries.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/layout.css') }}" >
+        <script type="text/javascript" src="{{ URL::asset('js/functions.js') }}"></script>
+        <title>Studentb&amp;b | @yield('title', 'Home')</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <!-- Header
+   ================================================== -->
+            <header>
+                <div class="row">
+                   <div class="twelve columns">
+                      <div class="logo">
+                         <a href="index.html"><img alt="" src="images/logo.png"></a>
+                      </div>
+
+                      <nav id="nav-wrap">
+                         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                         <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                        @include('navs/_navlocatore')
+                      </nav>
+                       <!-- end #nav-wrap -->
+                   </div>
+                </div>
+            </header> <!-- Header End -->
+
+            <!-- end #menu -->
+            <div>
+                @yield('content')
+            </div>
+
+            <!-- end #content -->
+            <footer>
+                @include('footer/_footer')
+            </footer> 
+            <!-- end #footer -->
+        </div>
     </body>
 </html>
