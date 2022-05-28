855f8a4a2376314421eb4626590b3c42c54edde7
Matteo Straccia <Matteo Straccia@LAPTOP-UDJNAOF5.home>
2022-05-28 12:12:59 +0200

    Modifica a view scrittMess + profilo Loc

--- laraProj6/resources/views/profiloLocatario.blade.php
+++ laraProj6/resources/views/profiloLocatario.blade.php
@@ -10,24 +10,33 @@
 
 <!-- Works Section
    ================================================== -->
-   <section id="works">
+   <section id="works_profilo">
 
-      <div class="row">
+      <div class="row align-center-div ">
           
-          <div class="column">
-            <h1>Profilo utente</h1>
-            
-            <div>
-            <p class="titolo">Nome: <span class="testo">{{ auth()->user()->nome }}</span></p>
-            <p class="titolo">Cognome: <span class="testo">{{ auth()->user()->cognome }}</span></p>
-            <p class="titolo">Sesso: <span class="testo">{{ auth()->user()->sesso }}</span></p>
-            <p class="titolo">Data di nascita: <span class="testo">{{ date('d-m-Y', strtotime(auth()->user()->data_nasc)) }}</span></p>
-            <p class="titolo">Username: <span class="testo">{{ auth()->user()->username }}</span></p>
+          <div class="column ">
+              <center>
+              <div class="crick"><h1>Profilo utente</h1></div>
+              
+                     <div>
+                         <img class="avatar_profilo" src="images/icona_utente.png" alt="">
+                    </div>
+                  
+                  
+                  
+                  <div id="box-profilo">
+            <p class="titolo piccolo-crick">Nome: <span class="testo">{{ auth()->user()->nome }}</span></p>
+            <p class="titolo piccolo-crick">Cognome: <span class="testo">{{ auth()->user()->cognome }}</span></p>
+            <p class="bordo_normale piccolo-crick"></p>
+            <p class="titolo piccolo-crick ">Sesso: <span class="testo">{{ auth()->user()->sesso }}</span></p>
+            <p class="titolo piccolo-crick ">Data di nascita: <span class="testo">{{ date('d-m-Y', strtotime(auth()->user()->data_nasc)) }}</span></p>
+            <p class="titolo piccolo-crick ">Username: <span class="testo">{{ auth()->user()->username }}</span></p>
             </div>
-
-            <button onclick="location.href = '{{ route('profiloLocModifica') }}';">Modifica i tuoi dati</button>
-
-    </div>
+                
+            <button class="crick" onclick="location.href = '{{ route('profiloLocModifica') }}';">Modifica i tuoi dati</button>
+             </center>
+          </div>
+       </div>
 
     </section> <!-- Works Section End-->
     
--- laraProj6/resources/views/scritturaMessaggio.blade.php
+++ laraProj6/resources/views/scritturaMessaggio.blade.php
@@ -10,9 +10,9 @@ currNavBar(3);
 
 <!-- Content
    ================================================== -->
-   <div class="content-outer">
+   <section id="info">
 
-      <div id="page-content2" class="row">
+      <div class="row">
 
          <div id="primary" class="twelve columns">
             
@@ -20,21 +20,25 @@ currNavBar(3);
             <div id="comments2">
                
                <h1>Rispondi a {{ $mess->mitt->nome }} {{ $mess->mitt->cognome }}</h1>
-               <h3>In merito all'alloggio:<p>{{ $mess->alloggio->nome }}<p></h3>
+               <h2>In merito all'alloggio:<p>{{ $mess->alloggio->nome }}<p></h2>
                <h5>Testo del messaggio ricevuto:<cite> "{{ $mess->testo }}"</cite></h5>
                
                {{ Form::open(array('route' => 'inviaMessLoc', 'class' => 'filters-form')) }}
                
                <div class="crick">
+                <div>
                 {{ Form::label('testo', 'Testo del messaggio', ['title' => 'Valore facoltativo']) }}
                 {{ Form::textarea('testo', '', ['id' => 'testo', 'placeholder' => 'Scrivi il tuo messaggio qua']) }}
-                
+               </div>
+               <div>
                 {{ Form::hidden('id_dest', $mess->mitt->id, ['id' => 'id_dest']) }}
                 {{ Form::hidden('id_alloggio', $mess->alloggio->id, ['id' => 'id_alloggio']) }}
-               
-                <center><div class="container-form-btn">                
+               </div>
+                <center>
+                <div class="container-form-btn">                
                 {{ Form::submit('Invia messaggio', ['class' => 'form-btn1']) }}
-                </div></center>
+                </div>
+                </center>
     
                 {{ Form::close() }}
 
@@ -45,5 +49,5 @@ currNavBar(3);
          </div>
 
          </div> <!-- Comments End -->
-
+         </section>
 @endsection
