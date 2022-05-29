9cdca769fe67ed3587b639ef190623207aa0575f
chris <s1093726@studenti.univpm.it>
2022-05-29 17:51:10 +0200

    Accettazione offerta locatore + inizio contratto

---
+++ laraProj6/resources/views/contratto.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>HouStudent | Contratto</title>
+    </head>
+    <body>
+        <h1>Titolo</h1>
+        <div class="row">
+            <div class="column">
+                <h2>Dati locatore</h2>
+            </div>
+            <div class="column">
+                <h2>Dati locatario</h2>
+            </div>
+        </div>
+    </body>
+</html>
