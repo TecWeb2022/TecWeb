2f7c4e71f24322a18e718076147bb30967ee245a
Luca <s1094518@studenti.univpm.it>
2022-06-14 10:49:12 +0200

    commenti su optionlist

--- laraProj6/app/Models/OptionList.php
+++ laraProj6/app/Models/OptionList.php
@@ -30,15 +30,20 @@ class OptionList
         return $opts;
     }
     
+    //effettua il join tra accommodation, users e options e restituisce tutte le opzioni relative a tutti gli alloggi di un locatore
     public function getAccsAndOpts($id_propr){
-            
+        
+        //siccome alcune colonne delle tabelle hanno lo stesso nome, come prima cosa vengono rinominate
         $accs = Accomodation::where('proprietario','=',$id_propr)
                                 ->select('id AS id_acc','nome AS nome_acc','tipologia AS tipologia_acc','canone', 'path_foto','proprietario','created_at AS created_as_acc','assegnato','inizio_disp','fine_disp');
             
         $opts = Option::select('id AS id_opt','data_stipula','id_alloggio','id_locatario','created_at AS created_at_opt');
             
         $locs = User::select('id AS id_loc','nome AS nome_loc','cognome AS cognome_loc','sesso','data_nasc');
-            
+        
+        //Per effettuare il join è necessario interpretare $accs come una tabella. Utilizziamo DB::raw che permette di eseguire una qualsiasi query.
+        //Il mergebinding permette di collegare le variabili (in questo caso $id_propr) richiamate dalle query precedenti, altrimenti andrebbero perse.
+        //Viene usato joinSub poichè permette di fare il join direttamente con una query e non obbligatoriamente una tabella.
         $accs_opts = DB::table(DB::raw("({$accs->toSql()}) as accs"))
                                 ->mergeBindings($accs->getQuery())
                                 ->joinSub($opts,'opts',function($join){
@@ -56,7 +61,7 @@ class OptionList
         return $accs_opts_locs;
     }
         
-        
+    //effettua il join tra accommodation, users e options e restituisce le opzioni relative ad un alloggio di un locatore
     public function getAccsAndOptsByAcc($id_propr,$id_acc){
             
         $accs = Accomodation::where('proprietario','=',$id_propr)
